0078ed96dfbab405232cc426dc1b25b080eea0a5
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-05-15 17:50:25 -0400

    api ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/categories' => 'categories#index', :as => 'categories'
+  get '/locations' => 'locations#index', :as => 'locations'
+  get '/users' => 'users#index', :as => 'users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
